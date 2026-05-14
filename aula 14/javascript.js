60f4da449810ae84d15623a562537576e8988ffe
Bernardo Vitral <vitral.tecnologia@gmail.com>
2026-05-13 22:34:25 -0300

    Add files via upload

---
+++ aula 14/javascript.js
@@ -0,0 +1,34 @@
+var a = 5 
+var b = 10 
+
+console.log(a===b)
+
+
+var x =10
+if (x > 5) {
+    console.log("x is greater than 5");
+}   else if (x<5){
+    console.log("x is less than 5");
+}   else {
+    console.log("x is equal to 5"); 'x is equal to 5'
+}
+
+for (let i = 0; i< 10; i += 2){
+    console.log(i); 
+}
+
+for (let i = 0; i< 10; i += 2){
+    console.log (i); 
+}
+
+var y = 0;
+while (y < 10){
+    console.log(y); 
+    y++;
+}
+
+y = 1
+while (y < 10){
+    (y)
+    y++;
+}
