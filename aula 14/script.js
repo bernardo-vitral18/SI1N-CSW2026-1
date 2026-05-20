404fc54131da40bcfdcb6ea683810602f3e19525
Bernardo Vitral <vitral.tecnologia@gmail.com>
2026-05-19 22:29:27 -0300

    Add files via upload

---
+++ aula 14/script.js
@@ -0,0 +1,80 @@
+var a = 5
+var b = 10
+
+console.log(a === b)
+
+
+var x = 10
+if (x > 5) {
+    console.log("x is greater than 5");
+} else if (x < 5) {
+    console.log("x is less than 5");
+} else {
+    console.log("x is equal to 5"); 'x is equal to 5'
+}
+
+for (let i = 0; i < 10; i += 2) {
+    console.log(i);
+}
+
+for (let i = 0; i < 10; i += 2) {
+    console.log(i);
+}
+
+var y = 0;
+while (y < 10) {
+    console.log(y);
+    y++;
+}
+
+y = 1
+while (y < 10) {
+    (y)
+    y++;
+}
+
+var issoEumaString = "Isso é uma String";
+let issoEumNumero = 42;
+const issoEumBooleano = true;
+let issoTambemEumaString = "42";
+
+if (issoEumNumero === issoTambemEumaString) {
+    console.log("As strings são iguais");
+} else {
+    console.log("As strings são diferentes");
+}
+
+console.log(issoEumNumero + issoTambemEumaString)
+
+console.log(issoEumNumero + issoEumNumero)
+
+var issoEumaGlobal = "Eu sou uma variável global";
+{
+    let issoEumaLocal = "Eu sou uma variável local";
+    console.log(issoEumaGlobal);
+    console.log(issoEumaLocal);
+}
+
+console.log(issoEumaGlobal);
+
+let floatvalue1 = 0.06;
+let floatvalue2 = 0.01;
+let sum = floatvalue1 + floatvalue2
+
+console.log(sum.toPrecision(2));
+
+for(let i = 0; i<5; i++) {
+    console.log(i);
+    console.log(i*2);
+    console.log(i*2+1);
+}
+
+let j = 0;
+while (j<5) {
+    console.log(j*2+1);
+    j++;
+}
+
+
+
+
